7808c6a4fc863d82b6343e24dbd9b7cb0396ec2c
Fırat Çoşkun <firatcoskun1733@gmail.com>
2025-06-16 01:11:26 +0300

    First Commit

---
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity; 
+
+namespace MvcKutuphane.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities(); //KütüphaneEntitiesta bulunan tablo ve propertieslere ulaşmamızı sağlıyor bu nesne. 
+
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KategoriEkle(TBLKATEGORI p)
+        {
+            db.TBLKATEGORI.Add(p);
+            db.SaveChanges();
+
+            return View();
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var kategori = db.TBLKATEGORI.Find(id);
+            db.TBLKATEGORI.Remove(kategori);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktg = db.TBLKATEGORI.Find(id);
+
+            return View("KategoriGetir", ktg);
+
+          
+
+        }
+
+
+        public ActionResult KategoriGuncelle(TBLKATEGORI p)
+        {
+            var ktg = db.TBLKATEGORI.Find(p.ID);
+            ktg.AD = p.AD;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+        
+    }
+}
---
+++ MvcKutuphane/Controllers/KitapController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+
+
+namespace MvcKutuphane.Controllers
+{
+    public class KitapController : Controller
+    {
+        // GET: Kitap
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var kitaplar = db.TBLKITAP.ToList();
+            return View(kitaplar);
+        }
+
+        [HttpPost]
+        public ActionResult KitapEkle()
+        {
+
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult KitapEkle(TBLKITAP p)
+        {
+
+            List<SelectListItem> deger1 = (from i in db.TBLKATEGORI.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.AD,
+                                               Value = i.ID.ToString()
+                                           }).ToList();
+
+            ViewBag.dgr1 = deger1;
+
+            List<SelectListItem> deger2 = (from i in db.TBLYAZAR.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.AD + ' ' + i.SOYAD,
+                                               Value = i.ID.ToString()
+                                           }).ToList();
+
+
+            ViewBag.dgr2 = deger2;
+
+
+            return View();
+        }
+    }
+}
