f4dfad3f94db2cdaa0cabd62e5d640c8fa5a99b7
Fırat Çoşkun <firatcoskun1733@gmail.com>
2025-07-04 15:29:26 +0300

    Implement login and register pages with authentication

---
+++ MvcKutuphane/Controllers/GirisController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+using System.Web.Security;
+
+namespace MvcKutuphane.Controllers
+{
+    public class GirisController : Controller
+    {
+        // GET: Giris
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult GirisYap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GirisYap(TBLUYELER p)
+        {
+            var bilgiler = db.TBLUYELER.FirstOrDefault(x => x.MAIL == p.MAIL && x.SIFRE == p.SIFRE);
+
+            if(bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.MAIL, false);
+                Session["Mail"] = bilgiler.MAIL.ToString();
+                //TempData["Id"] = bilgiler.ID.ToString();
+                //TempData["Ad"] = bilgiler.AD.ToString();
+                //TempData["Soyad"] = bilgiler.SOYAD.ToString();
+                //TempData["KullanıcıAdı"] = bilgiler.KULLANICIADI.ToString();
+                //TempData["Sifre"] = bilgiler.SIFRE.ToString();
+                //TempData["Uni"] = bilgiler.OKUL.ToString();
+                return RedirectToAction("Index","Panel");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+    }
+}
